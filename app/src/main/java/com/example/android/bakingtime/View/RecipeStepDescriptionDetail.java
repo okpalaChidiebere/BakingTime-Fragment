71bdbea912ed790f6542ac2ed12e00c8bf5ef07b
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-06-10 23:36:35 -0600

    Fixed Bug: Broken onclick Button

--- app/src/main/java/com/example/android/bakingtime/View/RecipeStepDescriptionDetail.java
+++ app/src/main/java/com/example/android/bakingtime/View/RecipeStepDescriptionDetail.java
@@ -1,16 +1,17 @@
 package com.example.android.bakingtime.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.android.bakingtime.Model.BakingStep;
 import com.example.android.bakingtime.R;
 
+import java.io.Serializable;
 import java.util.List;
 
 public class RecipeStepDescriptionDetail extends AppCompatActivity {
@@ -45,67 +46,70 @@ public class RecipeStepDescriptionDetail extends AppCompatActivity {
             fragmentManager.beginTransaction()
                     .add(R.id.player_view_and_description_body, playerViewFragment)
                     .commit();
+        }else{
+
+            /*to keep track of the list and index when the user uses the button to go through steps
+            even after screen configuration to watch video in full screen*/
+
+            bakingStep = (List<BakingStep>) savedInstanceState.getSerializable(BAKING_RECIPE_STEPS_LIST);
+            currentListIndex = savedInstanceState.getInt(LIST_INDEX);
         }
 
-        // The "Next" button launches a new fragment with next steps
-        Button nextButton = (Button) findViewById(R.id.tv_next_step);
-        nextButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                if(currentListIndex < bakingStep.size()) {
-
-                    playerViewFragment = new FragmentRecipePlayerView();
-                    playerViewFragment.setRecipeStepsList(bakingStep);
-
-                    Log.e("Index before add", ""+currentListIndex);
-
-                    /*int nextIndex;
-                    if(currentListIndex < 0)
-                        nextIndex = 0;
-                    else
-                        nextIndex = currentListIndex++;*/
-
-                    int nextIndex = currentListIndex++;
-
-                    Log.e("Index after increment", ""+nextIndex);
-
-                    playerViewFragment.setListIndex(nextIndex);
-
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.player_view_and_description_body, playerViewFragment)
-                            .commit();
-                }
-            }
-        });
-
-        // The "previous" button launches a new AndroidMeActivity
-        Button previousButton = (Button) findViewById(R.id.tv_previous_step);
-        previousButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(currentListIndex >= 0) {
-
-                    playerViewFragment = new FragmentRecipePlayerView();
-                    playerViewFragment.setRecipeStepsList(bakingStep);
-                    Log.e("Index before subtract", ""+currentListIndex);
-                    int previousIndex = currentListIndex--;
-                    Log.e("Index after subtract", ""+previousIndex);
-                    playerViewFragment.setListIndex(previousIndex);
-
-                    FragmentManager fragmentManager = getSupportFragmentManager();
-
-                    fragmentManager.beginTransaction()
-                            .replace(R.id.player_view_and_description_body, playerViewFragment)
-                            .commit();
-                }
-            }
-        });
 
     }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
 
+        outState.putSerializable(BAKING_RECIPE_STEPS_LIST, (Serializable) bakingStep);
+        outState.putInt(LIST_INDEX, currentListIndex);
 
+        super.onSaveInstanceState(outState);
+    }
+
+    // The "Next" button launches a new fragment with previous steps
+    public void previousStep(View view) {
+
+        if (currentListIndex != 0) {
+            currentListIndex = currentListIndex - 1;
+
+            playerViewFragment = new FragmentRecipePlayerView();
+            playerViewFragment.setRecipeStepsList(bakingStep);
+            playerViewFragment.setListIndex(currentListIndex);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.player_view_and_description_body, playerViewFragment)
+                    .commit();
+
+        }
+        else{
+            // Show an error message as a toast
+            Toast.makeText(this, "You are at the first step", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    // The "Next" button launches a new fragment with next steps
+    public void nextStep(View view) {
+
+        if(currentListIndex < bakingStep.size() - 1) {
+            currentListIndex = currentListIndex + 1;
+
+            playerViewFragment = new FragmentRecipePlayerView();
+            playerViewFragment.setRecipeStepsList(bakingStep);
+            playerViewFragment.setListIndex(currentListIndex);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            fragmentManager.beginTransaction()
+                    .replace(R.id.player_view_and_description_body, playerViewFragment)
+                    .commit();
+        }
+        else{
+            // Show an error message as a toast
+            Toast.makeText(this, "This is the last step", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
