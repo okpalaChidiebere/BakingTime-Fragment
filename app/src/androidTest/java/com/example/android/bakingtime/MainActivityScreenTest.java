b7525425f69e3b0096ae93cf29cf215ab9fa32f3
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-06-13 03:36:54 -0600

    Testing RecyclerViews with Espresso Recipe Ingredients and Steps
    The test we are running is clicking on a RecyclerView baking steps opens up the correct video. As well as also checking the ingredients displayed
    
    We know it opens up the correct recipe ingredients and baking instructions when recyclerview row text matches the name we specified. Test passed!

--- app/src/androidTest/java/com/example/android/bakingtime/MainActivityScreenTest.java
+++ app/src/androidTest/java/com/example/android/bakingtime/MainActivityScreenTest.java
@@ -27,6 +27,9 @@ import static org.hamcrest.core.AllOf.allOf;
 public class MainActivityScreenTest {
 
     public static final String BAKING_FOOD_TAG = "Yellow Cake";
+    public static final String SHORT_RECIPE_STEP_DESCRIPTION = "Recipe Introduction";
+    public static final String BAKING_INGREDIENT_NAME = "sifted cake flour";
+    public static final String BAKING_INGREDIENT_MEASUREMENT = "233.0G";
 
     /*Testing the AdapterViews for the RecyclerView in the MainActivity class*/
     @Rule
@@ -35,9 +38,11 @@ public class MainActivityScreenTest {
     /**
      * Clicks on a RecyclerView item and checks it opens up the RecipeName that loads the master
      * fragment with the correct ingredient and recipe step details.
+     *
+     * The we select a specific recipe baking step(instruction summary) and checks it opens up the RecipeStepDescription(full step instruction)
      */
     @Test
-    public void clickRecyclerViewItem_OpensRecipeNameActivity() {
+    public void clickRecyclerViewItem_OpensRecipeNameActivity_ClickAStepInstruction_OpensRecipeStepDescriptionDetailWithVideoAndInstruction() {
 
         // Get a reference to a specific RecyclerView item and clicks it.
         onView(withId(R.id.recyclerView_bakingFoodTags))
@@ -49,6 +54,19 @@ public class MainActivityScreenTest {
         onView(allOf(instanceOf(TextView.class),
                 withParent(withResourceName("action_bar"))))
                 .check(matches(withText(BAKING_FOOD_TAG)));
+
+        // Checks that the RecipeStepDescriptionDetail opens showing the correct baking ingredient displayed
+        onView(withId(R.id.tv_recyclerView_step_ingredients)).check(matches(hasDescendant(withText(BAKING_INGREDIENT_NAME))));
+        //onView(withId(R.id.tv_recyclerView_step_ingredients)).check(matches(hasDescendant(withText(BAKING_INGREDIENT_MEASUREMENT))));
+
+
+        // Get a reference to a specific RecyclerView instruction item and clicks it.
+        onView(withId(R.id.tv_recyclerView_step_description))
+                .perform(RecyclerViewActions.actionOnItem(
+                        hasDescendant(withText(SHORT_RECIPE_STEP_DESCRIPTION)), click()));
+
+        // Checks that the RecipeStepDescriptionDetail opens showing the correct step instruction displayed
+        onView(withId(R.id.tv_recipe_step_instruction)).check(matches(withText(SHORT_RECIPE_STEP_DESCRIPTION)));
 
     }
 
